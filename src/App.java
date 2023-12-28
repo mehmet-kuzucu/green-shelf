dab91723f6f0ff85f1a4f6ea0d0fca7fc2d3bef7
Ali Yiğit Taş <48025526+aliyigittas@users.noreply.github.com>
2023-12-29 00:03:20 +0300

    File structure changes

--- src/App.java
+++ src/App.java
@@ -16,7 +16,7 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
 
-
+//TODO:databaseAdapter.closeConnection(); // program kapatıldığında database bağlantısını kapatmayı unutma memory leak olmasın diye
 
 public class App extends Application {
 
@@ -32,70 +32,6 @@ public class App extends Application {
             // Load FXML files
             FXMLLoader scene = new FXMLLoader(getClass().getResource("app/greenshelf/fxml/loginPage.fxml"));
             /*
-            Parent root2 = FXMLLoader.load(getClass().getResource("fxml/homePage.fxml"));
-            Parent root3 = FXMLLoader.load(getClass().getResource("fxml/register_deneme.fxml"));
-            Parent root4 = FXMLLoader.load(getClass().getResource("fxml/register2_deneme.fxml"));
-            Parent root5 = FXMLLoader.load(getClass().getResource("fxml/customerHome.fxml"));
-            */
-
-            // Create scenes
-            /* 
-            scene2 = new Scene(root2);
-            scene3 = new Scene(root3);
-            scene4 = new Scene(root4);
-            scene5 = new Scene(root5);
-*/
-
-            /*
-            // Get the label from Scene 2
-            Label userInfoLabel = (Label) root2.lookup("#userInfoLabel");
-            Button logoutButton = (Button) root2.lookup("#logoutButton");
-
-            // Get the components from Scene 3
-            Button nextStepButton = (Button) root3.lookup("#nextStepButton");
-            Hyperlink existedAccount = (Hyperlink) root3.lookup("#existedaccount");
-
-            // Get the components from Scene 4
-            Hyperlink existedAccount2 = (Hyperlink) root4.lookup("#existedaccount");
-
-            // Get the components from Scene 5
-            Button registerButton = (Button) root4.lookup("#registerButton");
-
-            */
-
-            // when hyperlink (or Create new profile) is clicked
-            /* 
-            registerLink.setOnAction(e -> {
-                primaryStage.setScene(scene3);
-            });
-*/        /*
-            // when next step button is clicked
-            nextStepButton.setOnAction(e -> {
-                primaryStage.setScene(scene4);
-            });
-*/
-
-            /* 
-            // when existedaccount hyperlink is clicked
-            existedAccount.setOnAction(e -> {
-                primaryStage.setScene(scene1);
-            });
-*/          /* 
-            existedAccount2.setOnAction(e -> {
-                primaryStage.setScene(scene1);
-            });
-*/          /*
-            // when register button is clicked
-            registerButton.setOnAction(e -> {
-                primaryStage.setScene(scene5);
-            });
-*/
-            // spinner
-            
-
-            
-            
-            /*
             // submit butonuna basıldığında
             submitButton.setOnAction(e -> {
                 String username = usernameField.getText();
@@ -103,7 +39,6 @@ public class App extends Application {
                 FileChooser fileChooser = new FileChooser();
                 File selectedFile = fileChooser.showOpenDialog(primaryStage);
 
-                
                 //Burda userdan direkt class olusturmayacak aslinda once username ve passwordu databasete kontrol edecek sonra typeina gore admin, customer veya carier clasi olusturacak
                 currentUser = new User(username, password);
                 userInfoLabel.setText("HOSGELDIN " + currentUser.getUsername() + "!\nGiris bilgilerin:\nname: " + currentUser.getUsername() + ", password: " + currentUser.getPassword());
@@ -114,14 +49,6 @@ public class App extends Application {
             */
 
             databaseAdapter.generateSqlDatabase(); // db yoksa olusturuyor, varsa hicbir sey yapmıyor
-
-            // back butonuna basıldığında
-            /* 
-            logoutButton.setOnAction(e -> {
-                //databaseAdapter.closeConnection();
-                primaryStage.setScene(scene1);
-            });
-*/
             // Set the primary stage
             primaryStage.setScene(new Scene(scene.load()));
             primaryStage.setTitle("User Information Example");
