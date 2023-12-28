d891047656df8f645a943ebed006a6f7c808c42b
Ali Yiğit Taş <48025526+aliyigittas@users.noreply.github.com>
2023-12-29 00:21:57 +0300

    Update CSS file paths in registerPage.fxml and registerPage2.fxml

--- src/App.java
+++ src/App.java
@@ -1,19 +1,7 @@
-import java.io.File;
-
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
 
 
 //TODO:databaseAdapter.closeConnection(); // program kapatıldığında database bağlantısını kapatmayı unutma memory leak olmasın diye
