f31c900395f9c524ef6cc000a5adf3c095ffce7f
Cyclops9 <akbassbars99@hotmail.com>
2023-12-30 14:06:58 +0300

    controller

---
+++ src/app/greenshelf/controllers/customerHomeController.java
@@ -0,0 +1,23 @@
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+
+public class customerHomeController {
+
+    @FXML
+    private Button addToCartButton;
+
+    @FXML
+    private Group productGroup;
+
+    @FXML
+    private Button profilePhoto;
+
+    @FXML
+    private Button shoppingCartButton;
+
+    @FXML
+    private Spinner<?> spinner;
+
+}
