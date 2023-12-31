20c2561c15980a055be8eef3054793acb7bd35de
Ali Yiğit Taş <48025526+aliyigittas@users.noreply.github.com>
2023-12-31 22:36:56 +0300

    Update profileInfoPageController and profileInfoPage.fxml

--- src/app/greenshelf/controllers/profileInfoPageController.java
+++ src/app/greenshelf/controllers/profileInfoPageController.java
@@ -1,13 +1,13 @@
 package app.greenshelf.controllers;
 import java.io.ByteArrayInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.lang.ScopedValue.Carrier;
 import java.util.Base64;
 
-import app.greenshelf.Customer;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -15,11 +15,15 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
-import app.greenshelf.DatabaseAdapter;
-import app.greenshelf.controllers.registerPageController;
+import javafx.stage.Stage;
+import app.greenshelf.*;
 
 
 public class profileInfoPageController {
+
+    private Stage stage;
+    private Scene scene;
+    
     @FXML
     private Button saveProfileButton;
 
@@ -41,9 +45,13 @@ public class profileInfoPageController {
     @FXML
     private TextField changeEmailField;
 
+    @FXML
+    private ImageView greenShelfLogo;
+
+    @FXML
+    private Text editErrorText;
+
     private Customer currentUser;
-
-
 
 
     public void initData(Customer user) {
@@ -55,6 +63,34 @@ public class profileInfoPageController {
         changeAddressField.setText(currentUser.getAddress());
         changePhoneField.setText(currentUser.getPhone());
         changePasswordField.setText(currentUser.getPassword());
+    }
+
+    @FXML
+    void greenShelfLogoOnMouseClicked(MouseEvent event) {
+        loadScene("../fxml/customerHome.fxml", currentUser,null,null);
+    }
+
+    private void loadScene(String fxmlPath, Customer user, Admin admin, Carrier carrier) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+            
+            if (fxmlPath.equals("../fxml/customerHome.fxml")) {
+                customerHomeController controller = loader.getController();
+                controller.initData(user); // Pass the User object to the controller
+            }
+            if (fxmlPath.equals("../fxml/adminHomePage.fxml")) {
+                adminHomePageController controller = loader.getController();
+                controller.initData(admin); // Pass the User object to the controller
+            }
+            
+            stage = (Stage) greenShelfLogo.getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -71,11 +107,15 @@ public class profileInfoPageController {
         if(registerPageController.checkEmailisValid(changeEmailField) == false)
         {
             System.out.println("Email is not valid");
+            editErrorText.setText("Email is not valid");
+            changeEmailField.setStyle("-fx-border-color: red");
             return;
         }
         if(registerPageController.checkPhoneisValid(changePhoneField) == false)
         {
             System.out.println("Phone is not valid");
+            editErrorText.setText("Phone is not valid");
+            changePhoneField.setStyle("-fx-border-color: red");
             return;
         }
         databaseAdapter.updateInfo(new_password, new_email, new_address, new_phone, currentUser.getUsername());
