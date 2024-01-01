99366b605bfb8b9cd53081681827ec22029e1e37
Ali Yiğit Taş <48025526+aliyigittas@users.noreply.github.com>
2024-01-01 18:16:48 +0300

    Update shopping cart page controller and fxml

--- src/app/greenshelf/controllers/shoppingCartPageController.java
+++ src/app/greenshelf/controllers/shoppingCartPageController.java
@@ -6,9 +6,13 @@ import java.util.Base64;
 import app.greenshelf.Customer;
 import app.greenshelf.Product;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class shoppingCartPageController {
     @FXML
@@ -17,7 +21,12 @@ public class shoppingCartPageController {
     @FXML
     private ImageView profilePhotoImage;
 
+    @FXML
+    private ImageView greenShelfLogo;
+
     private Customer currentUser;
+    private Stage stage;
+    private Scene scene;
 
     public void initData(Customer user) {
         currentUser = user;
@@ -25,5 +34,27 @@ public class shoppingCartPageController {
         profilePhotoImage.setImage( new Image(new ByteArrayInputStream(Base64.getDecoder().decode(currentUser.getProfilePicture()))));
         //get products from shopping cart
         
+    }
+
+    @FXML
+    void greenShelfLogoOnMouseClicked() {
+        loadScene("../fxml/customerHome.fxml", currentUser);
+    }
+
+    private void loadScene(String fxmlPath, Customer user) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+            stage = (Stage) profilePhotoImage.getScene().getWindow();
+            customerHomeController controller = loader.getController();
+            controller.initData(user); // Pass the User object to the controller
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
